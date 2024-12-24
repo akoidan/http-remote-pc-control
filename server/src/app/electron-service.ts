d28cdfd5a2c63114c5d92687d2821456de476125
akoidan <deathangel908@gmail.com>
2024-12-24 22:33:20 +0100

    123

--- server/src/app/electron-service.ts
+++ server/src/app/electron-service.ts
@@ -6,13 +6,15 @@ import {
 export class ElectronService {
 
   async bootstrap(): Promise<void> {
+    console.log("Bootstraping electron service");
+    await app.whenReady();
     app.on('will-quit', () => {
       globalShortcut.unregisterAll()
     });
   }
 
   registerShortcut(shortCut: string, cb: () => Promise<void>): void {
-    const ret = globalShortcut.register(shortCut, () => cb);
+    const ret = globalShortcut.register(shortCut, cb);
     console.debug(`registering ${shortCut} shortcut`);
     if (!ret) {
       throw Error(`registration ${shortCut} failed`)
