405167d913f08f38c9916924a3c9d1e604342fb3
akoidan <deathangel908@gmail.com>
2025-01-15 23:30:23 +0100

    fix

--- client/src/keyboard/keyboard-service.ts
+++ client/src/keyboard/keyboard-service.ts
@@ -1,4 +1,7 @@
-import {Injectable} from '@nestjs/common';
+/*
+ eslint-disable no-await-in-loop
+ */
+import { Injectable } from '@nestjs/common';
 import {
   Key,
   keyboard,
@@ -7,7 +10,7 @@ import {
   InjectPinoLogger,
   PinoLogger,
 } from 'nestjs-pino';
-import {invertedMap} from '@/keyboard/keyboard-dto';
+import { invertedMap } from '@/keyboard/keyboard-dto';
 
 @Injectable()
 export class KeyboardService {
@@ -23,8 +26,28 @@ export class KeyboardService {
     await keyboard.type(keymap);
   }
 
+  private readonly specialCharacters = '$';
+
   async type(text: string): Promise<void> {
-    this.logger.info(`Type: \u001b[35m${text}`);
-    await keyboard.type(text);
+    if (text.includes(this.specialCharacters)) {
+      await this.typeWithSpecialCharacters(text);
+    } else {
+      this.logger.info(`Type: \u001b[35m${text}`);
+      await keyboard.type(text);
+    }
+  }
+
+  private async typeWithSpecialCharacters(text: string): Promise<void> {
+    const parts = text.split(this.specialCharacters);
+    for (let i = 0; i < parts.length; i++) {
+      this.logger.debug(`Type: \u001b[35m${text}`);
+      await keyboard.type(parts[i]);
+      if (i < parts.length - 1) {
+        this.logger.debug('Type: \u001b[35m$4');
+        await keyboard.pressKey(Key.LeftShift);
+        await keyboard.type('4');
+        await keyboard.releaseKey(Key.LeftShift);
+      }
+    }
   }
 }
