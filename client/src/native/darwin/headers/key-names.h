5f123eddbfbc2379daeff41f99e1323fc21b96dd
akoidan <deathangel908@gmail.com>
2025-02-07 03:43:16 +0100

    Pragma onces

--- client/src/native/darwin/headers/key-names.h
+++ client/src/native/darwin/headers/key-names.h
@@ -1,10 +1,7 @@
-#ifndef KEY_NAMES_H
-#define KEY_NAMES_H
+#pragma once
 
 #include <map>
 #include <string>
 #include <Carbon/Carbon.h>
 
 extern std::map<std::string, CGKeyCode> key_names;
-
-#endif // KEY_NAMES_H
--- client/src/native/darwin/headers/keypress.h
+++ client/src/native/darwin/headers/keypress.h
@@ -1,8 +1,5 @@
-#ifndef NATIVE_KEYPRESS_H
-#define NATIVE_KEYPRESS_H
+#pragma once
 
 #include "napi.h"
 
 Napi::Object keyboard_init(Napi::Env env, Napi::Object exports);
-
-#endif
