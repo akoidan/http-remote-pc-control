d729ceafbf80574a12d4090605fd0a3c01721361
akoidan <deathangel908@gmail.com>
2025-03-09 16:13:22 +0100

    Removed older code

---
+++ src/swagger.ts
@@ -0,0 +1,23 @@
+import {NestFactory} from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import {AppModule} from '@/app/app.module';
+import {writeFile} from 'fs/promises';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Http Remote PC controll API')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  await writeFile('./swagger.json', JSON.stringify(document, null, 2));
+}
+
+void bootstrap();
