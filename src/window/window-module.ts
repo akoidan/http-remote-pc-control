c10160d04139a56a213907f9cdfc7ab5f4fd60f9
akoidan <deathangel908@gmail.com>
2025-06-19 19:07:39 +0200

    sdf

--- src/window/window-module.ts
+++ src/window/window-module.ts
@@ -4,10 +4,8 @@ import {
 } from '@nestjs/common';
 import {WindowController} from '@/window/window-controller';
 import {WindowService} from '@/window/window-service';
-import {NativeModule} from '@/native/native-module';
 
 @Module({
-  imports: [NativeModule],
   controllers: [WindowController],
   providers: [
     Logger,
