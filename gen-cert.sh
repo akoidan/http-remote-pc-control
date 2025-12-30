5fdf9cbc5f7e55153c95b8590ed496743a11beea
akoidan <deathangel908@gmail.com>
2025-12-30 22:38:35 +0100

    asd

--- gen-cert.sh
+++ gen-cert.sh
@@ -1,36 +1,70 @@
 #!/bin/bash
 
 # Exit script on error
-set -xe
-mkdir -p ./gencert/client
-mkdir -p ./gencert/server
-mkdir -p ./gencert/ca
+set -e
 
-# Generate CA Key and Certificate
-openssl genrsa -out ./gencert/ca/ca-key.pem 2048
-openssl req -new -x509 -key ./gencert/ca/ca-key.pem -out ./gencert/ca/ca-cert.pem -days 3650 -subj "/C=US/ST=CA/L=SF/O=Example Org/CN=Example Root CA"
+OUT_DIR=./gencert
 
-# Generate Server Key and CSR
-openssl genrsa -out ./gencert/server/key.pem 2048
-openssl req -new -key ./gencert/server/key.pem -out ./gencert/server/csr.pem -subj "/C=US/ST=CA/L=SF/O=Example Org/CN=localhost"
 
-# Sign Server Certificate with CA including SAN
-openssl x509 -req -in ./gencert/server/csr.pem -CA ./gencert/ca/ca-cert.pem -CAkey ./gencert/ca/ca-key.pem -CAcreateserial -out ./gencert/server/cert.pem -days 365 -extfile san.cnf -extensions v3_req
+HRPC_CA_KEY=$OUT_DIR/ca/ca-key.pem
+HRPC_CA_CERT=$OUT_DIR/ca/ca-cert.pem
+SAN_CNF=./san.cnf
 
-# Generate Client Key and CSR
-openssl genrsa -out ./gencert/client/key.pem 2048
-openssl req -new -key ./gencert/client/key.pem -out ./gencert/client/csr.pem -subj "/C=US/ST=CA/L=SF/O=Example Org/CN=Client"
 
-# Sign Client Certificate with CA
-openssl x509 -req -in ./gencert/client/csr.pem -CA ./gencert/ca/ca-cert.pem -CAkey ./gencert/ca/ca-key.pem -CAcreateserial -out ./gencert/client/cert.pem -days 365
+chp(){
+    size=${#1}
+    indent=$((25 - $size))
+    printf "\e[1;37;40m$1\e[0;36;40m"
+    printf " "
+    for (( c=1; c<= $indent; c++))  ; do
+    printf "."
+    done
+    printf " \e[0;33;40m$2\n\e[0;37;40m"
+}
 
-cp ./gencert/ca/ca-cert.pem ./gencert/client/ca-cert.pem
-cp ./gencert/ca/ca-cert.pem ./gencert/server/ca-cert.pem
+genCa() {
+    mkdir -p $OUT_DIR/ca
+    set -x
+    openssl genrsa -out $HRPC_CA_KEY 2048
+    openssl req -new -x509 -key $HRPC_CA_KEY -out $HRPC_CA_CERT -days 3650 -subj "/C=US/ST=CA/L=SF/O=Example Org/CN=Example Root CA"
+    set +x
+}
 
-rm ./gencert/ca/ca-cert.pem
-rm ./gencert/ca/ca-cert.srl
+genClient() {
+    echo $1
+    HRPC_CLIENT_KEY=$OUT_DIR/server/key.pem
+    HRPC_CLIENT_CSR=$OUT_DIR/server/csr.pem
+    HRPC_CLIENT_CERT=$OUT_DIR/server/cert.pem
+    ## Generate Server Key and CSR
+    openssl genrsa -out $HRPC_CLIENT_KEY 2048
+    openssl req -new -key $HRPC_CLIENT_KEY -out $HRPC_CLIENT_CSR -subj "/C=US/ST=CA/L=SF/O=Example Org/CN=localhost"
+    ## Sign Server Certificate with CA including SAN
+    openssl x509 -req -in $HRPC_CLIENT_CSR -CA $HRPC_CA_CERT -CAkey $HRPC_CA_KEY -CAcreateserial -out $HRPC_CLIENT_CERT -days 365 -extfile $SAN_CNF -extensions v3_req
+    rm $HRPC_CLIENT_CSR
+}
 
-rm ./gencert/server/csr.pem
-rm ./gencert/client/csr.pem
+# Show help if no arguments
+show_help() {
+    chp ca "Creates CA (required to generate clients)"
+    chp client "Generates client from CA"
+    exit 1
+}
 
-echo "Done!"
+# Main script logic
+if [ $# -eq 0 ]; then
+    show_help
+fi
+
+
+case "$1" in
+    client)
+        genClient "$@"
+        ;;
+    ca)
+        genCa "$@"
+        ;;
+    *)
+        echo "Error: Unknown command '$cmd'"
+        show_help
+        ;;
+esac
