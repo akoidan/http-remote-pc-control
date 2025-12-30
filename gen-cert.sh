ec9f3c3e56dd7de1000766cf16f0f975c051528b
akoidan <deathangel908@gmail.com>
2025-12-30 22:51:35 +0100

    asd

--- gen-cert.sh
+++ gen-cert.sh
@@ -23,31 +23,46 @@ chp(){
 }
 
 genCa() {
+    if [ -f "$HRPC_CA_KEY" ]; then
+        echo "Error: CA key already exists at $HRPC_CA_KEY"
+        echo "To generate a new CA, first remove the existing CA files"
+        exit 1
+    fi
     mkdir -p $OUT_DIR/ca
     set -x
     openssl genrsa -out $HRPC_CA_KEY 2048
     openssl req -new -x509 -key $HRPC_CA_KEY -out $HRPC_CA_CERT -days 3650 -subj "/C=US/ST=CA/L=SF/O=Example Org/CN=Example Root CA"
     set +x
+    echo "CA certificate and key generated successfully in $OUT_DIR/ca/"
 }
 
 genClient() {
-    echo $1
-    HRPC_CLIENT_KEY=$OUT_DIR/server/key.pem
-    HRPC_CLIENT_CSR=$OUT_DIR/server/csr.pem
-    HRPC_CLIENT_CERT=$OUT_DIR/server/cert.pem
+    local client_name=$1
+    mkdir -p $OUT_DIR/$client_name
+    HRPC_CLIENT_KEY=$OUT_DIR/$client_name/key.pem
+    HRPC_CLIENT_CSR=$OUT_DIR/$client_name/csr.pem
+    HRPC_CLIENT_CERT=$OUT_DIR/$client_name/cert.pem
+    HRPC_CLIENT_CA_CERT=$OUT_DIR/$client_name/ca-cert.pem
+    echo "Generating client certificate in: $OUT_DIR/$client_name"
     ## Generate Server Key and CSR
+    set -x
     openssl genrsa -out $HRPC_CLIENT_KEY 2048
     openssl req -new -key $HRPC_CLIENT_KEY -out $HRPC_CLIENT_CSR -subj "/C=US/ST=CA/L=SF/O=Example Org/CN=localhost"
     ## Sign Server Certificate with CA including SAN
     openssl x509 -req -in $HRPC_CLIENT_CSR -CA $HRPC_CA_CERT -CAkey $HRPC_CA_KEY -CAcreateserial -out $HRPC_CLIENT_CERT -days 365 -extfile $SAN_CNF -extensions v3_req
+    cp $HRPC_CA_CERT $HRPC_CLIENT_CA_CERT
+    set +x
     rm $HRPC_CLIENT_CSR
+    echo Created $HRPC_CLIENT_KEY $HRPC_CLIENT_CERT
 }
 
 # Show help if no arguments
 show_help() {
     chp ca "Creates CA (required to generate clients)"
-    chp client "Generates client from CA"
-    exit 1
+    chp "client <name>" "Generates client certificate with given name"
+    chp clean "Removes $OUT_DIR"
+    chp all "Generates CA as well as client and server certificates"
+    chp 1
 }
 
 # Main script logic
@@ -58,10 +73,23 @@ fi
 
 case "$1" in
     client)
-        genClient "$@"
+        if [ -z "$2" ]; then
+            echo "Error: Client name is required"
+            show_help
+            exit 1
+        fi
+        genClient "$2"
         ;;
     ca)
-        genCa "$@"
+        genCa
+        ;;
+    clean)
+        rm -r $OUT_DIR
+        ;;
+    all)
+        genCa
+        genClient server
+        genClient client
         ;;
     *)
         echo "Error: Unknown command '$cmd'"
