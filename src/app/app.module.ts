25fd14c760e7681d9d21b618de38cadefe709769
akoidan <deathangel908@gmail.com>
2026-01-16 05:45:46 +0100

    Fixed mouse-move

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {Logger, MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import {AppController} from '@/app/app-controller';
 import {KeyboardModule} from '@/keyboard/keyboard-module';
-import {ExecuteModule} from '@/execute/execute-module';
 import {MouseModule} from '@/mouse/mouse-module';
 import {RequestIdMiddleware} from '@/app/request-id-middleware';
 import {WindowModule} from '@/window/window-module';
@@ -10,7 +9,7 @@ import {MonitorModule} from '@/monitor/monitor-module';
 import {ProcessModule} from '@/process/process-module';
 
 @Module({
-  imports: [KeyboardModule, ExecuteModule, MouseModule, WindowModule, MonitorModule, ProcessModule, NativeModule],
+  imports: [KeyboardModule, MouseModule, WindowModule, MonitorModule, ProcessModule, NativeModule],
   controllers: [AppController],
   providers: [Logger, RequestIdMiddleware],
 })
