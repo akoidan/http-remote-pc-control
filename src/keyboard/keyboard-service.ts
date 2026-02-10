77e6b365b77d7386961d8f84f88e9529505103d4
death <deathangel908@gmail.com>
2026-02-10 22:45:01 +0100

    ddfdd

--- src/keyboard/keyboard-service.ts
+++ src/keyboard/keyboard-service.ts
@@ -2,7 +2,7 @@ import {Inject, Injectable, Logger} from '@nestjs/common';
 import {KeyboardNativeModule, Native} from '@/native/native-model';
 import {sleep} from '@/shared';
 import {RandomService} from '@/random/random-service';
-import {KeyboardLayoutValue} from '@/keyboard/keyboard-dto';
+import {KeyboardLayoutValue, KeyPressRequest, SetKeyboardLayoutRequest, TypeTextRequest} from '@/keyboard/keyboard-dto';
 import {Safe400} from '@/utils/decorators';
 import {OS_INJECT} from '@/global/global-model';
 
@@ -20,45 +20,48 @@ export class KeyboardService {
 
 
   @Safe400(['win32', 'linux'])
-  public async typeText(text: string, delay?: number, deviationDelay?: number): Promise<void> {
-    this.logger.log(`Type: \u001b[35m${text}`);
-    if (delay) {
-      const realDelay = deviationDelay ? this.rs.calcDeviation(delay, deviationDelay) : delay;
-      for (const char of text.split('')) {
+  public async typeText(body: TypeTextRequest): Promise<void> {
+    this.logger.log(`Type: \u001b[35m${body.text}`);
+    if (body.keyDelay) {
+      let realDelay = body.keyDelay;
+      for (const char of body.text.split('')) {
+        if (body.keyDelayDeviation) {
+          realDelay =  this.rs.calcDeviation(body.keyDelay, body.keyDelayDeviation);
+        }
         await sleep(realDelay); // sleep before, in case we are typing on the same pc the shorcut was triggered from
         // to avoid meta keys in keystrokes
         this.addon.typeString(char);
       }
     } else {
-      this.addon.typeString(text);
+      this.addon.typeString(body.text);
     }
   }
 
   @Safe400(['win32', 'linux'])
-  public setLayout(layout: KeyboardLayoutValue): void {
-    this.addon.setKeyboardLayout(layout);
+  public setLayout(body: SetKeyboardLayoutRequest): void {
+    this.addon.setKeyboardLayout(body.layout);
   }
 
   @Safe400(['win32', 'linux'])
-  public async keyPress(keys: string[], holdKeys: string[], duration?: number): Promise<void> {
-    for (const key of holdKeys) {
+  public async keyPress(body: KeyPressRequest): Promise<void> {
+    for (const key of (body.holdKeys ?? [])) {
       this.logger.log(`HoldKey: \u001b[35m${key}`);
       // libnut.keyToggle(key, 'down', [])
       this.addon.keyToggle(key, [], true);
       await sleep(100);
     }
-    for (const key of keys) {
+    for (const key of body.keys) {
       this.logger.log(`KeyPress: \u001b[35m${key}`);
-      if (duration) {
+      if (body.duration) {
         this.addon.keyToggle(key, [], true);
-        await sleep(duration);
+        await sleep(body.duration);
         this.addon.keyToggle(key, [], false);
       } else {
         this.addon.keyTap(key, []);
       }
       await sleep(100);
     }
-    for (const key of holdKeys) {
+    for (const key of (body.holdKeys ?? [])) {
       this.logger.log(`ReleaseKey: \u001b[35m${key}`);
       this.addon.keyToggle(key, [], false);
       await sleep(100);
