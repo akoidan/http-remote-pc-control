1611a910d1080a7613bc333af6176b23ff27a5d5
akoidan <deathangel908@gmail.com>
2026-02-11 03:31:42 +0100

    asd

--- src/keyboard/keyboard-router.ts
+++ src/keyboard/keyboard-router.ts
@@ -1,16 +1,32 @@
-import {Mutation, Router} from 'nestjs-trpc';
-import {z} from 'zod';
+import {Input, Mutation, Router} from 'nestjs-trpc';
 import {KeyboardService} from '@/keyboard/keyboard-service';
-import {KeyPressRequestDto, keyPressRequestSchema} from '@/keyboard/keyboard-dto';
+import {
+  KeyPressRequestDto,
+  keyPressRequestSchema,
+  SetKeyboardLayoutRequestDto,
+  setKeyboardLayoutSchema,
+  TypeTextRequestDto,
+  typeTextRequestSchema,
+} from '@/keyboard/keyboard-dto';
 
 
 @Router({alias: 'keyboard'})
 export class KeyboardRouter {
   constructor(private readonly keyboardService: KeyboardService) {}
 
-  @Mutation({output: z.null(), input: keyPressRequestSchema})
-  async keyPress(input: KeyPressRequestDto): Promise<void> {
+  @Mutation({input: keyPressRequestSchema})
+  async keyPress(@Input() input:  KeyPressRequestDto): Promise<void> {
     await this.keyboardService.keyPress(input);
+  }
+
+  @Mutation({input: typeTextRequestSchema})
+  async typeText(@Input() body: TypeTextRequestDto): Promise<void> {
+    await this.keyboardService.typeText(body);
+  }
+
+  @Mutation({input: setKeyboardLayoutSchema})
+  setLayout(@Input() body: SetKeyboardLayoutRequestDto): void {
+    this.keyboardService.setLayout(body);
   }
 }
 
