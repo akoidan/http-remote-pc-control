5dbf36f2440befab4d2ffbdec0f22f6ed5402907
akoidan <deathangel908@gmail.com>
2026-02-17 21:27:19 +0100

    asd

--- tests/certificate-service.spec.ts
+++ tests/certificate-service.spec.ts
@@ -1,11 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { Logger } from '@nestjs/common';
-import { CertificateService } from '../src/certificates/certificate-service';
 import { createMockLogger } from './test-utils';
 import forge from 'node-forge';
+import {TlsService} from "../src/mtls/tls-service";
 
-describe('CertificateService', () => {
-  let service: CertificateService;
+describe('TlsService', () => {
+  let service: TlsService;
   let mockLogger: ReturnType<typeof createMockLogger>;
 
   beforeEach(async () => {
@@ -13,12 +13,12 @@ describe('CertificateService', () => {
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        CertificateService,
+        TlsService,
         { provide: Logger, useValue: mockLogger },
       ],
     }).compile();
 
-    service = module.get<CertificateService>(CertificateService);
+    service = module.get<TlsService>(TlsService);
   });
 
   describe('ca', () => {
@@ -57,29 +57,23 @@ describe('CertificateService', () => {
     });
 
     it('should create a client certificate signed by CA', () => {
-      const clientData = service.client('test-client', caData.key, caData.cert);
+      const clientData = service.client(caData.key, caData.cert);
       
       expect(clientData.key).toMatch(/^-----BEGIN PRIVATE KEY-----/);
       expect(clientData.cert).toMatch(/^-----BEGIN CERTIFICATE-----/);
       expect(clientData.caCert).toBe(caData.cert);
     });
 
-    it('should throw error when client name is not provided', () => {
-      expect(() => service.client('', caData.key, caData.cert)).toThrow('Error: output directory name is required');
-      expect(() => service.client(null as any, caData.key, caData.cert)).toThrow('Error: output directory name is required');
-      expect(() => service.client(undefined as any, caData.key, caData.cert)).toThrow('Error: output directory name is required');
-    });
-
     it('should create different client certificates for different names', () => {
-      const client1 = service.client('client1', caData.key, caData.cert);
-      const client2 = service.client('client2', caData.key, caData.cert);
+      const client1 = service.client( caData.key, caData.cert);
+      const client2 = service.client( caData.key, caData.cert);
       
       expect(client1.key).not.toBe(client2.key);
       expect(client1.cert).not.toBe(client2.cert);
     });
 
     it('should create client certificates with hardcoded values', () => {
-      const clientData = service.client('test-client', caData.key, caData.cert);
+      const clientData = service.client(caData.key, caData.cert);
       
       const cert = forge.pki.certificateFromPem(clientData.cert);
       const subject = cert.subject.attributes;
@@ -91,35 +85,4 @@ describe('CertificateService', () => {
     });
   });
 
-  describe('all', () => {
-    it('should create CA and two clients (server and client)', () => {
-      const allData = service.all();
-      
-      // Check CA
-      expect(allData.ca.key).toMatch(/^-----BEGIN PRIVATE KEY-----/);
-      expect(allData.ca.cert).toMatch(/^-----BEGIN CERTIFICATE-----/);
-      
-      // Check server client
-      expect(allData.server.key).toMatch(/^-----BEGIN PRIVATE KEY-----/);
-      expect(allData.server.cert).toMatch(/^-----BEGIN CERTIFICATE-----/);
-      expect(allData.server.caCert).toBe(allData.ca.cert);
-      
-      // Check client
-      expect(allData.client.key).toMatch(/^-----BEGIN PRIVATE KEY-----/);
-      expect(allData.client.cert).toMatch(/^-----BEGIN CERTIFICATE-----/);
-      expect(allData.client.caCert).toBe(allData.ca.cert);
-    });
-
-    it('should create different data on subsequent calls', () => {
-      const all1 = service.all();
-      const all2 = service.all();
-      
-      expect(all1.ca.key).not.toBe(all2.ca.key);
-      expect(all1.ca.cert).not.toBe(all2.ca.cert);
-      expect(all1.server.key).not.toBe(all2.server.key);
-      expect(all1.server.cert).not.toBe(all2.server.cert);
-      expect(all1.client.key).not.toBe(all2.client.key);
-      expect(all1.client.cert).not.toBe(all2.client.cert);
-    });
-  });
 });
